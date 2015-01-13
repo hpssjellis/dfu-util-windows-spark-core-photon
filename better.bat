e0c79b7f18adf450c3fdffc7eff3404958ca265f
Jeremy Ellis <jellis@hpssapps.com>
2015-01-12 23:52:22 -0800

    Update better.bat

--- better.bat
+++ better.bat
@@ -11,44 +11,55 @@ set DFU_FILES_PATH=c:\progra~1\APPLIC~1\npm;
 set PATH=C:\Windows;C:\Windows\System32;$NODE_PATH;$ARM_PATH;$SPARKCLI_PATH;$DFU_FILES_PATH
 
 
-
-
-
 REM note the next line overwrites the myoutput using > normally >> adds to the file
 
-echo "*************************************" >myoutput.txt 2>&1
-echo "node --version" >>myoutput.txt 2>&1
-node --version    >>myoutput.txt 2>&1
+echo "*************************************" > myoutput.txt 2>&1
+echo "$PATH" >> myoutput.txt 2>&1
+$PATH    >> myoutput.txt 2>&1
 
 
 
 
 
-echo "*************************************" >>myoutput.txt 2>&1
-echo "npm --version " >>myoutput.txt 2>&1
-npm --version    >>myoutput.txt 2>&1
-
-
-
-
-echo "*************************************" >>myoutput.txt 2>&1
-echo "spark-cli -version " >>myoutput.txt 2>&1
-spark-cli --version   >>myoutput.txt 2>&1
-
-
-
-
-echo "*************************************" >>myoutput.txt 2>&1
-echo "$DFU_FILES_PATH/dir " >>myoutput.txt 2>&1
-$DFU_FILES_PATH/dir    >>myoutput.txt 2>&1
+echo "*************************************" > myoutput.txt 2>&1
+echo "node --version" >> myoutput.txt 2>&1
+node --version    >> myoutput.txt 2>&1
 
 
 
 
 
-echo "*************************************" >>myoutput.txt 2>&1
-echo "spark flash --usb cc3000 " >>myoutput.txt 2>&1
-spark flash --usb cc3000    >>myoutput.txt 2>&1
+echo "*************************************" >> myoutput.txt 2>&1
+echo "npm --version " >> myoutput.txt 2>&1
+npm --version    >> myoutput.txt 2>&1
+
+
+
+
+echo "*************************************" >> myoutput.txt 2>&1
+echo "spark-cli -version " >> myoutput.txt 2>&1
+spark-cli --version   >> myoutput.txt 2>&1
+
+
+
+
+echo "*************************************" >> myoutput.txt 2>&1
+echo "$DFU_FILES_PATH/dir " >> myoutput.txt 2>&1
+$DFU_FILES_PATH/dir    >> myoutput.txt 2>&1
+
+
+
+echo "*************************************" >> myoutput.txt 2>&1
+echo "GCC ???? " >> myoutput.txt 2>&1
+dir    >> myoutput.txt 2>&1
+
+
+
+
+
+echo "*************************************" >> myoutput.txt 2>&1
+echo "spark flash --usb cc3000 " >> myoutput.txt 2>&1
+spark flash --usb cc3000    >> myoutput.txt 2>&1
 
 
 

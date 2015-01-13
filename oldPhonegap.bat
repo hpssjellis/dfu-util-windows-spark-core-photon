90bf7ef7a32616557d8ca6a9bf6bbcf0b652ffb7
Jeremy Ellis <jellis@hpssapps.com>
2015-01-13 15:12:34 -0800

    Create oldPhonegap.bat

---
+++ oldPhonegap.bat
@@ -0,0 +1,19 @@
+Net use p: "\\myNetwork\HPS Classes$\computer programming 2011\Assignments" /persistent:no
+
+
+set ANT_HOME=p:phonegapmain\ant
+set ANDROID_HOME=p:phonegapmain\android
+set JAVA_HOME=p:phonegapmain\java
+
+set PATH=C:\Windows;C:\Windows\System32;p:phonegapmain\java\bin;p:phonegapmain\android\tools;p:phonegapmain\android\platform-tools;p:phonegapmain\ant\bin
+
+
+
+set CLASSPATH=
+
+
+
+
+
+p:phonegapmain\phonegap\lib\android\bin\create.bat H:\Documents\aEllis2012_2013\cp2012\phonegap\myPhoneGap\myNew2 com.myone.myNew2 myNew2
+pause
